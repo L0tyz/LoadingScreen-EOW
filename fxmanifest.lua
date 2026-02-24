fb0e192afe412231e162df024958cba6c1800c01
L0tyz <l0tyz.pro@gmail.com>
2026-02-23 21:07:53 -0500

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'L0tyz'
+description 'End of Watch Loading'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+	'script.js',
+    'video/background.mp4',
+	'audio/music.mp3'
+}
+
+client_script 'client.lua'
